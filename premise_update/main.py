5c21dfc2f612315b476914b32f12af4af421f784
Jaume Triay <jaume.triay@baxterplanning.com>
2024-05-05 20:09:38 +0200

    Added simple login creation and changed the readme a little, also removed tests folder for the moment, since for now testing is not a priority.

--- premise_update/main.py
+++ premise_update/main.py
@@ -1,10 +1,16 @@
 from flask import Flask, render_template
+from premise_update.login.login_routes import login_blueprint, login_manager
 
 app = Flask(__name__)
+app.register_blueprint(login_blueprint)
+app.secret_key = 'my template secret key'  # need to replace with actual key
+login_manager.init_app(app)
+
 
 @app.route('/')
 def home():
     return render_template('placeholder.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
